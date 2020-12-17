1e2f0ad320b1a81a4cdc52b7fe1f9b6e4ed03628
Trevor Kimenye <kimenye@gmail.com>
2020-12-18 00:39:19 +0300

    Initial commit of the home page (without react)

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from 'react';
+import Routes from './routes';
+
+function App() {
+  return (
+    <Fragment>
+      <main>
+        <Routes />
+      </main>
+    </Fragment>
+  );
+}
+
+export default App;

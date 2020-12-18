5e5c7ee7d508731c7b5ce4bbe2f43b1ec32facd2
Trevor Kimenye <kimenye@gmail.com>
2020-12-18 15:47:41 +0300

    Added register form

--- client/src/components/Hero.js
+++ client/src/components/Hero.js
@@ -1,4 +1,30 @@
 import React, { Fragment } from 'react';
+import Button from './common/Button';
+
+const Feature = ({ text }) => {
+  return (
+    <li className="flex items-start">
+      <span className="h-6 flex items-center sm:h-7">
+        <svg className="flex-shrink-0 h-5 w-5 text-purple-600" viewBox="0 0 20 20" fill="currentColor">
+          <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd" />
+        </svg>
+      </span>
+      <p className="ml-2">
+        { text }
+      </p>
+    </li>
+  )
+}
+
+const SignUpForm = () => {
+  return (
+    <form>
+      <input type="email" placeholder="jane@example.com" required="true" className="mt-1 block w-full px-0.5 border-0 border-b-2 border-purple-200" />
+      <Button text='Sign Up' />
+
+    </form>
+  )
+}
 
 function Hero() {
   return (
@@ -8,44 +34,21 @@ function Hero() {
         <div className="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20">
           <div className="max-w-md mx-auto">
             <div>
-              <h1 className="logo text-purple-600">Fleetly</h1>
+              <h1 className="logo text-purple-600 text-4xl">Fleetly</h1>
             </div>
             <div className="divide-y divide-gray-200">
               <div className="py-4 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7">
-                <p>Fleet like a pro. Fleetly helps you to:</p>
+                <p>Fleet like a pro.</p>
                 <ul className="list-disc space-y-2">
-                  <li className="flex items-start">
-                    <span className="h-6 flex items-center sm:h-7">
-                      <svg className="flex-shrink-0 h-5 w-5 text-purple-600" viewBox="0 0 20 20" fill="currentColor">
-                        <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd" />
-                      </svg>
-                    </span>
-                    <p className="ml-2">
-                      Find your best performing Tweets by engagement
-                    </p>
-                  </li>
-                  <li className="flex items-start">
-                    <span className="h-6 flex items-center sm:h-7">
-                      <svg className="flex-shrink-0 h-5 w-5 text-purple-600" viewBox="0 0 20 20" fill="currentColor">
-                        <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd" />
-                      </svg>
-                    </span>
-                    <p className="ml-2">
-                      Schedule the best time to post them
-                    </p>
-                  </li>
-                  <li className="flex items-start">
-                    <span className="h-6 flex items-center sm:h-7">
-                      <svg className="flex-shrink-0 h-5 w-5 text-purple-600" viewBox="0 0 20 20" fill="currentColor">
-                        <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd" />
-                      </svg>
-                    </span>
-                    <p className="ml-2">Spend your time doing more productive things</p>
-                  </li>
+                  <Feature text={ 'Find your best engaged Tweets' } />
+                  <Feature text={ 'Schedule the best time to post them' } />
+                  <Feature text={ 'Spend your time doing more productive things' } />
                 </ul>
               </div>
-              <div className="pt-6 text-base leading-6 font-bold sm:text-lg sm:leading-7">
-                <p className="text-purple-600">Coming Soon!</p>
+              <div className="pt-6 text-base leading-6">
+                <p className="text-gray-700 text-small">Fleetly is currently in early access. Drop your email below
+                to be one of our early beta testers</p>
+                <SignUpForm />
               </div>
             </div>
           </div>

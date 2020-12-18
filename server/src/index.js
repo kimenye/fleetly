26c21c5e5f8988470e08c79d10656fb5da4bdf01
Trevor Kimenye <kimenye@gmail.com>
2020-12-18 09:34:27 +0300

    Added hello to server index

---
+++ server/src/index.js
@@ -0,0 +1,31 @@
+const Koa = require("koa");
+const BodyParser = require("koa-bodyparser");
+const Router = require("koa-router");
+const Logger = require("koa-logger");
+const serve = require("koa-static");
+const mount = require("koa-mount");
+const cors = require('koa-cors');
+const HttpStatus = require("http-status");
+
+const app = new Koa();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(BodyParser());
+app.use(Logger());
+app.use(cors());
+
+const router = new Router();
+
+router.get('/', async (ctx, next) => {
+
+  ctx.status = HttpStatus.OK;
+  ctx.body = 'Hello from Fleetly';
+  await next();
+
+})
+
+app.use(router.routes()).use(router.allowedMethods());
+app.listen(PORT, function () {
+    console.log("==> 🌎  Listening on port %s. Visit http://localhost:%s/", PORT, PORT);
+});

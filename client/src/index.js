586bb0d64af50f3b4c00ace4c42671258ded5050
Trevor Kimenye <kimenye@gmail.com>
2020-12-18 11:33:58 +0300

    Added tailwind and moved home page to App component

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
-import App from './App.js';
+
+import App from './App';
+import './tailwind.output.css';
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
